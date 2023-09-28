0e41a89e0e591d34dfaa81a67d28ad3cd0c04ab3
Fábio Guedes <59146075+fabionguedes@users.noreply.github.com>
2023-09-28 17:32:58 -0300

    Update and rename Exemplo 1.sql to GROUP BY + JOIN.sql

--- GROUP BY + JOIN.sql
+++ GROUP BY + JOIN.sql
@@ -1,6 +1,6 @@
 -- GROUP BY + JOIN
 --1.a) Crie um agrupamento mostrando o total de vendas (SalesQuantity) por ano (CalendarYear)
---1.b) Considere apenas o mês (CalendarMonthLabel) de 'January'
+--1.b) Considere apenas o mÃªs (CalendarMonthLabel) de 'January'
 --1.c) Na tabela resultante, mostre apenas os anos com um total de vendas maior ou igual a 1200000
 
 --1.a)
